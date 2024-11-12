9fae2d476d7c89ff8ae3916761ce2dc2a39947e8
Paweł Kołodziej <pawelkolodziej81@gmail.com>
2024-11-13 00:39:15 +0100

    Porting v-bind in-browser exercise result to SPA root component.

---
+++ 01.spa.api.options/01.porting.v-bind.ex/README.md
@@ -0,0 +1,35 @@
+# 01.porting.v-bind.ex
+
+This project demonstrates simply porting in-browser v-bind excercise progress bar component into SPA component. Primary commit demonstrates porting component "logic" to root component. Second commit demonstrates components reusability (including defining props for components).
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vite.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ 01.spa.api.options/01.porting.v-bind.ex/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper">
+    <div
+      class="progress"
+      role="progressbar"
+      aria-label="Success example"
+      aria-valuenow="progress"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style="height: 50px"
+    >
+      <div
+        class="progress-bar"
+        :class="[progressCss]"
+        :style="{
+          width: `${progress}%`,
+          transition: `width ${stepDuration}ms ease`,
+        }"
+      >
+        {{ progress }}%
+      </div>
+    </div>
+    <button @click="start()" class="btn btn-dark mt-2">START</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timerId: null,
+      progress: 0,
+      stepDuration: 50,
+    }
+  },
+  computed: {
+    progressCss() {
+      return this.progress >= 21
+        ? this.progress >= 51
+          ? 'bg-danger'
+          : 'bg-warning text-dark'
+        : 'bg-dark'
+    },
+  },
+  methods: {
+    start() {
+      this.progress = 0
+      clearInterval(this.timerId)
+      this.timerId = setInterval(() => {
+        if (this.progress === 100) {
+          clearInterval(this.timerId)
+        } else {
+          this.progress++
+        }
+      }, this.stepDuration)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  padding: 1rem;
+}
+</style>
